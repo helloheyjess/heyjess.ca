b35d0d7d9bac7887cbb924fdd6bb37611a8d9082
helloheyjess <jessicamayblack@gmail.com>
2015-04-26 19:31:41 -0400

    Updates to code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,14 +7,9 @@ $(function(){
 
 	// Trigger Scroll Events
 	$(window).scroll(function(){
-	
-		//Get scroll location percentage
-		var windowHeight = $(window).height();
-		var scrollHeight = $(window).scrollTop();
-		var scrollPercentage =  (scrollHeight / windowHeight);
 		
 		//When scrolled past header, add the alternate class to nav bar
-		if(scrollPercentage > 0.4) {
+		if($(window).scrollTop() > 400) {
 			$('body').addClass('scrolling');
 			$('.navBar').addClass('slideInDown');
 		} else {
